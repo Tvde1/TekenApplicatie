54281169054071a1f90e6019bb613c4819df6a5e
Tvde1 <tim3345@live.nl>
2017-09-20 15:47:35 +0200

    created test

---
+++ Tests/DrawingTest.java
@@ -0,0 +1,38 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DrawingTest {
+    @Test
+    public void getName() throws Exception {
+    }
+
+    @Test
+    public void setName() throws Exception {
+    }
+
+    @Test
+    public void getItems() throws Exception {
+    }
+
+    @Test
+    public void addOval() throws Exception {
+    }
+
+    @Test
+    public void addPaintedText() throws Exception {
+    }
+
+    @Test
+    public void addPolygon() throws Exception {
+    }
+
+    @Test
+    public void addImage() throws Exception {
+    }
+
+    @Test
+    public void clear() throws Exception {
+    }
+
+}
