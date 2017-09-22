7239a271e845c20890725338aed12e941f087d06
Tvde1 <tim3345@live.nl>
2017-09-22 11:25:51 +0200

    observing/observable etc

--- src/Drawing.java
+++ src/Drawing.java
@@ -3,16 +3,14 @@ import Objects.*;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Observable;
 
-public class Drawing {
+public class Drawing extends Observable {
     private ArrayList<DrawingItem> _items = new ArrayList<>();
     private String _name;
 
-    DrawingListener _listener;
-
-    Drawing(String name, DrawingListener listener) {
+    Drawing(String name) {
         _name = name;
-        _listener = listener;
     }
 
     String getName() { return _name; }
@@ -26,29 +24,40 @@ public class Drawing {
     void addOval(Color color, Point anchor, double width, double height, double weight) {
         Oval oval = new Oval(color, anchor, width, height, weight);
         _items.add(oval);
-        _listener.update();
+        setChanged();
+        notifyObservers();
     }
 
     void addPaintedText(Color color, Point anchor, double width, double height, String content, String fontName) {
         PaintedText paintedText = new PaintedText(color, anchor, width, height, content, fontName);
         _items.add(paintedText);
-        _listener.update();
+        setChanged();
+        notifyObservers();
     }
 
     void addPolygon(Color color, Point[] vertices, double weight) {
         Polygon polygon = new Polygon(color, vertices, weight);
         _items.add(polygon);
-        _listener.update();
+        setChanged();
+        notifyObservers();
     }
 
     void addImage(Color color, File file, Point anchor, double width, double height) {
         Image image = new Image(color, file, anchor, width, height);
         _items.add(image);
-        _listener.notify();
+        setChanged();
+        notifyObservers();
     }
 
     void clear() {
         _items.clear();
-        _listener.update();
+        setChanged();
+        notifyObservers();
+    }
+
+    void remove(DrawingItem item) {
+        _items.remove(item);
+        setChanged();
+        notifyObservers();
     }
 }
--- src/DrawingApp.java
+++ src/DrawingApp.java
@@ -8,28 +8,36 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-import javax.swing.*;
-import java.util.ArrayList;
+import java.io.File;
+import java.util.*;
 
-public class DrawingApp extends Application implements DrawingListener {
+public class DrawingApp extends Application implements Observer {
     private Scene _scene;
     private Drawing _drawing;
 
+    /**
+     * @param primaryStage
+     * @throws Exception
+     */
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
         primaryStage.setTitle("Hello World");
-        Scene scene = new Scene(root, 300, 275);
+        Scene scene = new Scene(root, 800, 500);
         _scene = scene;
         primaryStage.setScene(scene);
         primaryStage.show();
-        fillShapeDropdown(root);
+        fillShapeDropdown();
+        fillColorDropdown();
+        addButtonEvents();
 
-        _drawing = new Drawing("Drawing1", this);
+        _drawing = new Drawing("Drawing1");
+        _drawing.addObserver(this);
 
         _drawing.addOval(Color.BLUE, new Point(30, 4), 30, 50, 10);
         _drawing.addOval(Color.BLUE, new Point(4, 5), 20, 10, 5);
@@ -42,15 +50,32 @@ public class DrawingApp extends Application implements DrawingListener {
         };
 
         _drawing.addPolygon(Color.GREEN, vertices, 40);
-        _drawing.clear();
-        _drawing.addPolygon(Color.BLUE, vertices, 40);
+        //_drawing.clear();
+        //_drawing.addPolygon(Color.BLUE, vertices, 40);
 
+        _drawing.addPaintedText(Color.RED, new Point(400, 200), 400, 400, "Test", "Comic Sans MS");
+
+        _drawing.addImage(Color.BLUE, new File("D:\\temp\\old-man-with-a-phone.jpg"), new Point(100, 100), 200, 150);
         // setMouseClickHandler(scene);
+    }
+
+    private void addButtonEvents() {
+        Button btnAdd = (Button) _scene.getRoot().lookup("#btnAdd");
+        btnAdd.setOnMouseClicked(this::btnAdd_Click);
+
+        Button btnRemove = (Button) _scene.getRoot().lookup("#btnRemove");
+        btnRemove.setOnMouseClicked(this::btnRemove_Click);
     }
 
     public static void main(String[] args) { launch(args); }
 
-    private void fillShapeDropdown(Parent root) {
+    private void showItems() {
+        ArrayList<DrawingItem> items = _drawing.getItems();
+        javafx.scene.control.ListView lv = (javafx.scene.control.ListView) _scene.getRoot().lookup("#lvItems");
+        lv.getItems().setAll(items);
+    }
+
+    private void fillShapeDropdown() {
         ArrayList<String> items = new ArrayList<>();
         items.add("Image");
         items.add("Oval");
@@ -59,37 +84,73 @@ public class DrawingApp extends Application implements DrawingListener {
 
         ObservableList<String> list = FXCollections.observableArrayList(items);
 
-        ChoiceBox cb = (ChoiceBox) root.lookup("#cbShape");
+        ChoiceBox cb = (ChoiceBox) _scene.getRoot().lookup("#cbShape");
 
         if (cb != null) cb.setItems(list);
         else throw new Error("Combobox missing.");
     }
 
-    private void setMouseClickHandler(Scene scene) {
-        scene.setOnMousePressed(event -> {
-
-            System.out.println(event.getX());
-
-            JOptionPane.showMessageDialog(null, "Info", "InfoBox: " + "Titel", JOptionPane.INFORMATION_MESSAGE);
-        });
+    private void fillColorDropdown() {
+        ChoiceBox cb = (ChoiceBox) _scene.getRoot().lookup("#cbColor");
+        cb.getItems().setAll(Color.values());
     }
 
     private void draw() {
         Pane pane = (Pane) _scene.getRoot().lookup("#pane");
         pane.getChildren().clear();
 
-        for (DrawingItem item : _drawing.getItems()) {
-            Object s = item.draw();
-            if (s instanceof Node) {
-                pane.getChildren().add((Node) s);
-            } else {
-                //pane.getChildren().add((Image) s);
-            }
-        }
+        for (DrawingItem item : _drawing.getItems())
+            pane.getChildren().add(item.draw());
     }
 
     @Override
-    public void update() {
+    public void update(Observable o, Object arg) {
         draw();
+        showItems();
+    }
+
+    void btnRemove_Click(MouseEvent mouseEvent) {
+        Parent r = _scene.getRoot();
+        Node l =  r.lookup("#lvItems");
+        ListView lv = (ListView) l;
+        DrawingItem item = (DrawingItem) lv.getSelectionModel().getSelectedItem();
+        _drawing.remove(item);
+    }
+
+    void btnAdd_Click(MouseEvent mouseEvent) {
+        ChoiceBox cbShape = (ChoiceBox) _scene.getRoot().lookup("#cbShape");
+        ChoiceBox cbColor = (ChoiceBox) _scene.getRoot().lookup("#cbColor");
+        double x = Double.parseDouble(((TextField)_scene.getRoot().lookup("#tbX")).getText());
+        double y = Double.parseDouble(((TextField)_scene.getRoot().lookup("#tbY")).getText());
+        double width = Double.parseDouble(((TextField)_scene.getRoot().lookup("#tbWidth")).getText());
+        double height = Double.parseDouble(((TextField)_scene.getRoot().lookup("#tbHeight")).getText());
+        double weight = ((Slider) _scene.getRoot().lookup("#slWeight")).getValue();
+
+        TextField tbFile = (TextField) _scene.getRoot().lookup("#tbFile");
+        File file = new File(tbFile.getText());
+
+        Color c = (Color) cbColor.getSelectionModel().getSelectedItem();
+        Point anchor = new Point(x, y);
+
+        String text = ((TextField) _scene.getRoot().lookup("#tbText")).getText();
+        String font = ((TextField) _scene.getRoot().lookup("#tbFont")).getText();
+
+        switch ((String) cbShape.getSelectionModel().getSelectedItem()) {
+            case "Image": {
+                _drawing.addImage(c, file, anchor,width, height);
+                break;
+            }
+            case "Oval": {
+                _drawing.addOval(c, anchor, width, height, weight);
+                break;
+            }
+            case "Text": {
+                _drawing.addPaintedText(c, anchor, width, height, text, font);
+                break;
+            }
+            case "Polygon": {
+                break;
+            }
+        }
     }
 }
